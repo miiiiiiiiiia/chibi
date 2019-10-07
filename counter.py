733a877299f63341d7cf04c2b8d3f665b868e7d1
FUMIMOTO Ami <21716083@jwu.ac.jp>
2019-10-07 15:51:17 +0900

    Class counter

---
+++ counter.py
@@ -0,0 +1,28 @@
+
+class Counter(object):
+    def __init__(self): #コンストラクタ
+        self.cnt = 0
+
+    def count(self):
+        self.cnt += 1
+
+    def doublecount(self):
+        self.cnt += 2 #2回カウントする
+
+    def reset(self):
+        self.cnt = 0
+
+    def show(self):
+        print(self.cnt)
+
+    def __repr__(self): #文字列を返すと表示される
+        return str(self.cnt)
+
+
+
+c = Counter()
+c.show()
+c.doublecount()
+c.show()
+print(type(c))
+print(c)
