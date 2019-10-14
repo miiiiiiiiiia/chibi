7fdc5df76b7bf16ae742622f0c6008fbc2e1af48
FUMIMOTO Ami <21716083@jwu.ac.jp>
2019-10-14 16:42:52 +0900

    Commit'Expr'

--- exp.py
+++ exp.py
@@ -1,6 +1,8 @@
+class Expr(object):
+    pass
 
-class Val(object):
-    __slots__= ['value']
+class Val(Expr):
+    __slots__ = ['value']
     def __init__(self, value = 0):
         self.value = value
     def __repr__(self):
@@ -12,19 +14,31 @@ v = Val(1)
 print(v)
 assert v.eval() == 1
 
-class Add(object):
+assert isinstance(v, Expr) # ==> True
+assert isinstance(v, Val) # ==> True
+assert not isinstance(v, int)
+
+def toExpr(a):
+    if not isinstance(a, Expr):
+        a = Val(a)
+    return a
+
+class Add(Expr):
     __slots__=['left', 'right']
     def __init__(self, a, b):
-        self.left = a
-        self.right = b
+        self.left = toExpr(a) 
+        self.right = toExpr(b)
     def eval(self):
         return self.left.eval() + self.right.eval()
 
-e = Add(Val(1), Val(2))   #1+2
-print(e.eval())
-assert e.eval() == 3
 
-e = Add(Val(1),Add(Val(2),Val(3)))  #1+2+3
+e = Add(1,Add(1,2))
 print(e.eval())
+assert e.eval() == 4
 
+e = Add(Val(1),Add(Val(2),Val(3)))
+print(e.eval())
+assert e.eval() == 6
+
+print()
 print()
